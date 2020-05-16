d1de52dcc79e929dfe5b5a7ab0d7f8a43899c05d
geyth <65451799+geyth@users.noreply.github.com>
2020-05-16 16:55:34 +0300

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def homePageView(request):
-    return HttpResponse('Hello, World!')
+    return HttpResponse('Hello, World!!')
